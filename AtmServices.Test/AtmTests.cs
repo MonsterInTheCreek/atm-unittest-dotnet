7976de1a00387d869338d9b06b7e8305a971783b
David Turner <76234472+MonsterInTheCreek@users.noreply.github.com>
2025-01-25 01:42:30 +0000

    Added atm classes and tests

---
+++ AtmServices.Test/AtmTests.cs
@@ -0,0 +1,48 @@
+namespace AtmServices.Test;
+
+using AtmServices;
+
+public class AtmTests
+{
+    // Test fixture
+    Atm testAtm;
+    int initialBalance = 100;
+
+    public AtmTests()
+    {
+
+        testAtm = new Atm(initialBalance);
+
+    }
+
+    [Fact]  // test success
+    public void Test_Withdraw()
+    {
+        var result = testAtm.withdraw(25);
+        Assert.True(result);
+
+        Assert.Equal(75, testAtm.getBalance());
+    }
+
+    [Fact]  // test failure
+    public void Test_Withdraw_Failure()
+    {
+        var result = testAtm.withdraw(125);
+        Assert.False(result);
+
+        Assert.Equal(100, testAtm.getBalance());
+    }
+
+    [Fact]  // sanity test
+    public void Test_Withdraw_And_Deposit()
+    {
+        var result = testAtm.withdraw(25);
+        Assert.True(result);
+
+        var resultOfDeposit = testAtm.deposit(25);
+        Assert.True(resultOfDeposit);
+
+        Assert.Equal(100, testAtm.getBalance());
+    }
+
+}
